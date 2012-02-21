97b9c4c605774dfaf2243eda7f5f69baf584d54c
Peter Zsoldos <peter.zsoldos@gmail.com>
2012-02-21 01:15:02 -0800

    vmware tools ubuntu script added

---
+++ bin/install-vmware-tools-ubuntu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo mount /dev/cdrom1 /media/cdrom
+cp /media/cdrom/VMwareTools-*.tar.gz /tmp/
+sudo umount /media/cdrom/
+cd /tmp
+tar xzvf VMwareTools-*.tar.gz
+cd vmware-tools-distrib/
+kernel_version=`uname -a | cut -f 3 -d ' '`
+headers_to_install=`apt-cache search  header | grep "^linux-headers-$kernel_version" | cut -f 1 -d ' '`
+sudo aptitude install build-essential gcc binutils make $headers_to_install
+sudo ./vmware-install.pl -d
+sudo /etc/init.d/networking stop
+sudo rmmod pcnet32
+sudo rmmod vmxnet
+sudo modprobe vmxnet
+sudo /etc/init.d/networking start
