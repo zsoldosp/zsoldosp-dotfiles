1a74e5949ade7cbfa7d19051ee371f45c95cacdd
zsoldosp <peter.zsoldos@gmail.com>
2012-04-07 05:12:15 -0700

    converted to functions, moved one level up

---
+++ .heroku-helpers.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+function init-heroku-env() {
+    sudo apt-get install python-pip python-dev build-essential 
+    sudo pip install --upgrade pip 
+    sudo pip install --upgrade virtualenv 
+    heroku_install_file=/tmp/heroku-toolbelt.sh
+    wget -q https://toolbelt.heroku.com/install.sh -O $heroku_install_file
+    chmod 755 $heroku_install_file
+    $heroku_install_file
+    heroku login
+}
+
+function init-heroku-project() {
+    rm -rf ~/$DJANGO_PROJECT; mkdir ~/$DJANGO_PROJECT; cd ~/$DJANGO_PROJECT
+    virtualenv venv --distribute
+    source venv/bin/activate
+    git init
+    for to_ignore in venv "*.pyc" "*~" "*.swp"; do
+        echo "$to_ignore" >> .gitignore
+    done
+    git init
+    git add .gitignore
+    git commit -m"basic gitignore file"
+}
--- bin/install-heroku.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-sudo apt-get install python-pip python-dev build-essential 
-sudo pip install --upgrade pip 
-sudo pip install --upgrade virtualenv 
-heroku_install_file=/tmp/heroku-toolbelt.sh
-wget -q https://toolbelt.heroku.com/install.sh -O $heroku_install_file
-chmod 755 $heroku_install_file
-$heroku_install_file
-heroku login
