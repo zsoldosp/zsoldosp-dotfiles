e5d4b6178721b3b2585027948dc4340930fc41d1
Peter Zsoldos <peter.zsoldos@gmail.com>
2012-05-27 07:32:04 -0700

    git config steps added

--- install.sh
+++ install.sh
@@ -2,8 +2,12 @@
 
 function ubuntu-needed-packages() {
     sudo apt-get install openssh-server vim git-core
+    git config --global user.name "Peter Zsoldos"
+    (git config --global user.email | grep peter\.zsoldos) || echo "need to setup your git user.email setting! Use git config --global user.email 'someone@somewhere.tld'"
+
 }
 
+ubuntu-needed-packages
 dir_name=`pwd`/`dirname $0`
 ls -a $dir_name | while read file; do
     if [[ "$file" == '.' ]]; then
