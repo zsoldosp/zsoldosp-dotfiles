b81c839dd0568fe27101b86c20fd1c2eb5953ef5
Peter Zsoldos <peter.zsoldos@gmail.com>
2012-05-27 07:34:32 -0700

    reminder added to install keys on github

--- install.sh
+++ install.sh
@@ -3,7 +3,13 @@
 function ubuntu-needed-packages() {
     sudo apt-get install openssh-server vim git-core
     git config --global user.name "Peter Zsoldos"
-    (git config --global user.email | grep peter\.zsoldos) || echo "need to setup your git user.email setting! Use git config --global user.email 'someone@somewhere.tld'"
+    (git config --global user.email | grep peter\.zsoldos) || (echo "need to setup your git user.email setting! Use git config --global user.email 'someone@somewhere.tld'" && exit 1)
+    if [ ! -f ~/.ssh/id_rsa.pub ]; then
+        echo "setting up RSA.pub key for github"
+        email=`git config --global user.email`
+        ssh-keygen -t rsa -C "$email"
+    fi 
+    echo "Go to https://github.com/settings/ssh to add it github!"
 
 }
 
