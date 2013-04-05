a8aea8421ba04be7a62ef58af3b0641916f16ebd
Peter Zsoldos <peter.zsoldos@gmail.com>
2013-04-05 17:31:36 +0200

    More detailed failure messages and respecting the 80 char width

--- bin/snippets/python/django-test-client-bug-repro-1.3.py
+++ bin/snippets/python/django-test-client-bug-repro-1.3.py
@@ -9,12 +9,14 @@ class TestClientDoesNotClearRaisedExceptions(TestCase):
         class RaiseExceptionView(View):
             def get(self, request, should_raise=None):
                 if should_raise:
-                    raise ValueError('This view was configured to raise an exception')
+                    raise ValueError('This view was configured to raise')
                 return HttpResponse('Hello world!')
 
     urlpatterns = patterns('',
-        (r'raise-exception', views.RaiseExceptionView.as_view(), dict(should_raise=True), 'raise'),
-        (r'no-exception', views.RaiseExceptionView.as_view(), dict(should_raise=False), 'noraise'),
+        (r'raise-exception', views.RaiseExceptionView.as_view(),
+            dict(should_raise=True), 'raise'),
+        (r'no-exception', views.RaiseExceptionView.as_view(),
+            dict(should_raise=False), 'noraise'),
     )
 
     @classonlymethod
@@ -23,12 +25,21 @@ class TestClientDoesNotClearRaisedExceptions(TestCase):
         super(TestClientDoesNotClearRaisedExceptions, cls).setUpClass()
 
     def test_repro_from_the_end_users_perspective(self):
-        self.assertEquals(200, self.client.get('/no-exception/').status_code, 'as a first request, no exception is raised')
-        self.assertRaises(ValueError, lambda: self.client.get('/raise-exception/'))
-        self.assertEquals(200, self.client.get('/no-exception/').status_code, 'despite the previous request raising an exception, I should be able to perform another request')
+        self.assertEquals(200, self.client.get('/no-exception/').status_code, 
+            'as a first request, no exception is raised')
+        with self.assertRaises(ValueError): 
+            self.client.get('/raise-exception/'))
+        self.assertEquals(200, self.client.get('/no-exception/').status_code,
+            """despite the previous request raising an exception, I should be
+                able to perform another request with the same cleint""")
 
     def test__root_cause(self):
-        self.assertRaises(ValueError, lambda: self.client.get('/raise-exception/'))
-        self.assertEquals(None, self.client.exc_info, 'should have cleared client.exc_info, but did not - it is %s' % self.client.exc_info)
+        with self.assertRaises(ValueError): 
+            self.client.get('/raise-exception/'))
+        self.assertEquals(None, self.client.exc_info, """should have cleared 
+            client.exc_info, but did not - exception is raised/propagated by
+            middleware and thus never reaches the point in the request method
+            where it cleares it and then raises the exception (which was stored
+            there by the exception signal"""
 
 
