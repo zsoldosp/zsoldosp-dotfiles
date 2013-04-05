ce82dfd48ced6effb5c8f30e80ab6bf6fbd44326
Peter Zsoldos <peter.zsoldos@gmail.com>
2013-04-05 12:58:05 +0200

    added a repro for a test error we had in our system. Will review this at home to see whether it's only our setup or a generic django issue (and whether it persists in 1.5 too)

---
+++ bin/snippets/python/django-test-client-bug-repro-1.3.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from django.utils.decorators import classonlymethod
+from django.views.generic.base import View
+from django.conf.urls.defaults import patterns
+from django.http import HttpResponse
+
+class TestClientDoesNotClearRaisedExceptions(TestCase):
+    class views:
+        class RaiseExceptionView(View):
+            def get(self, request, should_raise=None):
+                if should_raise:
+                    raise ValueError('This view was configured to raise an exception')
+                return HttpResponse('Hello world!')
+
+    urlpatterns = patterns('',
+        (r'raise-exception', views.RaiseExceptionView.as_view(), dict(should_raise=True), 'raise'),
+        (r'no-exception', views.RaiseExceptionView.as_view(), dict(should_raise=False), 'noraise'),
+    )
+
+    @classonlymethod
+    def setUpClass(cls):
+        cls.urls = cls
+        super(TestClientDoesNotClearRaisedExceptions, cls).setUpClass()
+
+    def test_after_raised_exception_can_get_next(self):
+        self.assertEquals(200, self.client.get('/no-exception/').status_code, 'as a first request, no exception is raised')
+        self.assertRaises(ValueError, lambda: self.client.get('/raise-exception/'))
+        self.assertEquals(200, self.client.get('/no-exception/').status_code, 'despite the previous request raising an exception, I should be able to perform another request')
+
