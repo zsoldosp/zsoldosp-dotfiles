19c9e3d776dba787caeae4a5670181ca39835aae
zsoldosp <peter.zsoldos@gmail.com>
2013-07-22 18:59:53 +0200

    in django 1.3, the time the model is declared, it is registered

--- bin/snippets/python/django-declare-apps-inside-testcase-class-itself/declaremodelsinsidetests/testlib/tests/__init__.py
+++ bin/snippets/python/django-declare-apps-inside-testcase-class-itself/declaremodelsinsidetests/testlib/tests/__init__.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
+from django.db import models 
 
 
-class HelloWorld(TestCase):
-    def test_hello_world(self):
-        self.fail('hello')
+class ExploringDjangoModelRegistrationBehavior(TestCase):
+    def test_when_model_is_declared_it_is_registerd_but_not_in_installed_app(self):
+        models_before_declaring_class = set(models.get_models())
+        class FooModel(models.Model):
+            pass
+        models_after_declaring_class = set(models.get_models())
+        self.assertEquals(len(models_before_declaring_class) + 1, len(models_after_declaring_class))
+        self.assertEquals(set([FooModel]), models_after_declaring_class - models_before_declaring_class)
