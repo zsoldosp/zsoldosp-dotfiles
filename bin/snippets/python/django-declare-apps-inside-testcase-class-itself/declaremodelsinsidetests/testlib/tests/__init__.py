444446443836d5fdbc347d9f86dd28c516ee2923
zsoldosp <peter.zsoldos@gmail.com>
2013-07-22 19:25:31 +0200

    documenting how app_labels are derived by default

--- bin/snippets/python/django-declare-apps-inside-testcase-class-itself/declaremodelsinsidetests/testlib/tests/__init__.py
+++ bin/snippets/python/django-declare-apps-inside-testcase-class-itself/declaremodelsinsidetests/testlib/tests/__init__.py
@@ -1,12 +1,30 @@
 from django.test import TestCase
 from django.db import models 
+from django.conf import settings
+import sys
 
+LIBRARY_NAME = 'testlib'
 
-class ExploringDjangoModelRegistrationBehavior(TestCase):
-    def test_when_model_is_declared_it_is_registerd_but_not_in_installed_app(self):
-        models_before_declaring_class = set(models.get_models())
+class ModelsAreRegisteredWhenDeclared(TestCase):
+    def setUp(self):
+        super(ModelsAreRegisteredWhenDeclared, self).setUp()
+        self.original_model_classes = set(models.get_models())
+        
+    def test_model_registered_when_declared(self):
         class FooModel(models.Model):
             pass
         models_after_declaring_class = set(models.get_models())
-        self.assertEquals(len(models_before_declaring_class) + 1, len(models_after_declaring_class))
-        self.assertEquals(set([FooModel]), models_after_declaring_class - models_before_declaring_class)
+        self.assertEquals(len(self.original_model_classes) + 1, len(models_after_declaring_class))
+        self.assertEquals(set([FooModel]), models_after_declaring_class - self.original_model_classes)
+
+    def test_app_label_is_by_default_derives_from_module_regardsless_of_class_level_nesting(self):
+        cls2app_label = lambda cls: sys.modules[cls.__module__].__name__.split('.')[-2]
+        self.assertEquals(LIBRARY_NAME, cls2app_label(ModelsAreRegisteredWhenDeclared), 'if the test helper lambda does not work, no point proceeding further')
+        class NotNested(models.Model):
+            pass
+        self.assertEquals(cls2app_label(ModelsAreRegisteredWhenDeclared), NotNested()._meta.app_label)
+        class appname:
+            class models:
+                class NestedModel(models.Model):
+                    pass
+        self.assertEquals(cls2app_label(ModelsAreRegisteredWhenDeclared), appname.models.NestedModel()._meta.app_label)
