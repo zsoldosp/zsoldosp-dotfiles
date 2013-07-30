89641c8840ae7db3bf5d4ca42881566912fee511
Peter Zsoldos <peter.zsoldos@gmail.com>
2013-07-30 12:30:49 +0200

    urlresolver debug session frustration recorded witht he unhelpful (for beginnner urlresolver) message

---
+++ bin/snippets/python/django-urlresolver-why-does-it-not-tell-me-i-am-missing-the-leading-slash.py
@@ -0,0 +1,22 @@
+from django.conf.urls.defaults import *
+from django.http import HttpResponseRedirect
+
+class myurlpatterns:
+    urlpatterns = patterns('',
+        url(r'^hello', lambda *a, **kw: HttpResponseRedirect('/'), {}, 'foo'),
+    )
+
+from django.core import urlresolvers 
+from paessler.activation import urls
+orig_root_urlconf = settings.ROOT_URLCONF
+settings.ROOT_URLCONF = 'paessler.activation.urls'
+urlresolvers.clear_url_caches()
+should_match = urlresolvers.resolve('/hello')
+assert should_match is not None
+try:
+    unexpected_match = urlresolvers.resolve('hello')
+    assert Falase, 'should have raised, but got %r' % unexpected_match
+except urlresolvers.Resolver404:
+    # why not a meaningful text?
+    
+
