977a12a42d49fa2b1907c55ec73827bb9bc3d2e4
Peter Zsoldos <peter.zsoldos@gmail.com>
2014-01-22 11:51:57 +0100

    enhanced it to accept multiple classes

--- bin/snippets/python/combining-classes-genearating-new-ones-via-decorator.py
+++ bin/snippets/python/combining-classes-genearating-new-ones-via-decorator.py
@@ -15,29 +15,51 @@ class Blue(object):
 
 
 import sys
+import itertools
 
 
-def runwith(*classes):
-    """
-    Unconditionally skip a test.
-    """
+def runwith(*class_combinations):
+
+    def to_class(existing, new):
+        class Tmp(existing, new):
+            pass
+        Tmp.__name__ = ''.join([existing.__name__, new.__name__])
+        return Tmp
+
+
+    def flatten(listOfLists):
+        "Flatten one level of nesting"
+        return itertools.chain.from_iterable(listOfLists)
+
+            
+
     def decorator(target_cls):
-        for mixin_cls in classes:
-            class Combo(mixin_cls, target_cls):
-                pass
-            Combo.__name__ = ''.join([mixin_cls.__name__, target_cls.__name__])
-            setattr(sys.modules[target_cls.__module__], Combo.__name__, Combo)
+        outstanding = list(class_combinations)
+        existing = [target_cls]
+        while outstanding:
+            curr = outstanding.pop()
+            existing = list(
+                to_class(c, e)
+                for e in existing
+                for c in curr
+            )
+        for cls in existing:
+            setattr(sys.modules[target_cls.__module__], cls.__name__, cls)
         return target_cls
+
     return decorator
 
 
-@runwith(First, Second)
-class Foo(object):
+@runwith(
+    (First, Second),
+    (Red, Blue),
+)
+class Demo(object):
     def data(self):
-        return (self.index, type(self))
+        return (self.index, self.color, type(self))
 
 
-print SecondFoo().data()
+print SecondRedDemo().data()
 
 
 
